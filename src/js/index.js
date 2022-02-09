cb3a84aac614849d57741c9aab799a043865ec1d
Proyko Proykov <prkk@mail.bg>
2022-02-09 20:33:47 +0200

    carousel functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import '@/styles/index.scss'
+import Siema from 'siema';
+
+const siemaSlider = new Siema({
+  selector: '.testimonials-carousel-container',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: true,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
+
+document.querySelector('.carousel-control.left').addEventListener('click', () => siemaSlider.prev());
+document.querySelector('.carousel-control.right').addEventListener('click', () => siemaSlider.next());
